1338989cf3ac179b88d406f517fe8684f34d5641
KinoKiru <youriboulanger@gmail.com>
2022-12-12 00:52:34 +0100

    owo push

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -3,10 +3,9 @@ use poise::Command;
 
 mod age;
 mod join;
+mod leave;
+mod play;
 
 pub fn commands() -> Vec<Command<Data, Error>> {
-    return vec![
-        age::age(),
-        join::join(),
-    ];
+    return vec![age::age(), join::join(), leave::leave(), play::play()];
 }
