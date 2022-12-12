1a8fe95ad6c2aebb39f40ce9062702dc6cfd3a15
hubble459 <geraldd459@gmail.com>
2022-12-12 14:42:05 +0100

    /help

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -2,8 +2,9 @@ use crate::{Data, Error};
 use poise::Command;
 
 mod age;
+mod help;
 mod music;
 
 pub fn commands() -> Vec<Command<Data, Error>> {
-    return vec![age::age(), music::music()];
+    return vec![age::age(), music::music(), help::help()];
 }
