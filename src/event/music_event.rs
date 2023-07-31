35582eeeb9c924644bc0bb0b2d8547bd8c2d1b50
Kinokiru <youriboulanger@gmail.com>
2023-07-31 23:10:59 +0200

    added embed when song is finished playing

--- src/event/music_event.rs
+++ src/event/music_event.rs
@@ -3,17 +3,14 @@ use crate::{
     util::{self},
 };
 use poise::serenity_prelude::*;
-use songbird::{
-    tracks::{PlayMode, TrackHandle, TrackState},
-    EventContext, EventHandler as SongbirdEvent,
-};
+use songbird::{EventContext, EventHandler as SongbirdEvent};
 
 pub struct ClientHandler;
 
 #[async_trait]
 impl EventHandler for ClientHandler {
     async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+        println!("{} is ready!", ready.user.name);
     }
 
     async fn voice_state_update(&self, ctx: Context, _old: Option<VoiceState>, _new: VoiceState) {
@@ -37,34 +34,28 @@ pub struct TrackEndHandler {
 #[async_trait]
 impl SongbirdEvent for TrackEndHandler {
     async fn act(&self, ctx: &songbird::EventContext<'_>) -> Option<songbird::Event> {
-        // TODO MIGHT be only the song which ended, so if statement needs to be reworked
-        // TODO This means that track_list will always be len of 1
         if let EventContext::Track(track_list) = ctx {
-            println!("{:#?}", track_list);
+            let (manager, _) = util::manager::get_manager(&self.guild, &self.author, &self.context)
+                .await
+                .unwrap();
             if track_list.len() == 0 {
-                let (manager, _) =
-                    util::manager::get_manager(&self.guild, &self.author, &self.context)
-                        .await
-                        .unwrap();
                 command::music::leave::leave_channel(manager, self.guild.clone()).await;
             } else {
-                let numbers = track_list
-                    .into_iter()
-                    .filter(|n| n.0.playing != PlayMode::End)
-                    .collect::<Vec<&(&TrackState, &TrackHandle)>>();
+                let handler_lock = manager.get(self.guild.id).unwrap();
+                let handler = handler_lock.lock().await;
+                let queue = handler.queue();
 
                 self.channel_id
-                    .say(
-                        self.context.http(),
-                        Embed::fake(|f| {
+                    .send_message(self.context.http(), |e| {
+                        e.embed(|f| {
                             command::music::play::send_music_embed(
-                                Box::new(numbers[0].1.metadata().clone()),
+                                Box::new(queue.current().unwrap().metadata().clone().to_owned()),
                                 f,
                                 &self.author,
                             );
                             f
-                        }),
-                    )
+                        })
+                    })
                     .await
                     .unwrap();
             }
